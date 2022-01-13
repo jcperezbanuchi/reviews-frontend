baba610a3b9e9dffde5a007a0c4c88b3e63ba2e1
Juan C Perez Banuchi <juanco1935@gmail.com>
2022-01-13 01:01:34 -0400

    created a react app and added css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import ReviewDetails from "./pages/ReviewDetails";
+import Category from "./pages/Category";
+import SiteHeader from "./components/SiteHeader";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<SiteHeader />
+				<Routes>
+					<Route exact path="/" element={<Homepage />} />
+					<Route path="/details/:id" element={<ReviewDetails />} />
+					<Route path="/category/:id" element={<Category />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
